1a2bb30727a99cb858f23c7c01dec6c268940bf8
dongdigua <dongdigua@outlook.com>
2024-02-23 16:38:36 +0800

    fix alarm linear id; implement PRG

--- lib/alarmclock/router.ex
+++ lib/alarmclock/router.ex
@@ -7,31 +7,41 @@ defmodule Alarmclock.Router do
 
   get "/" do
     f = render_html()
-    send_resp(conn, 200, f)
+    conn
+    |> put_resp_content_type("text/html")
+    |> send_resp(200, f)
   end
 
-  post "/" do
+  post "/api/add" do
     cron = conn.params["cron"]
     song = conn.params["song"]
-    delete = conn.params["delete"]
-    cond do
-      cron != nil and song != nil ->
-        case Crontab.CronExpression.Parser.parse(cron) do
-          {:ok, expr} -> 
-            if song in Alarmclock.Song.songs(), do: add_alarm(expr, song)
-        end
-      delete != nil ->
-        Alarmclock.Scheduler.delete_job(String.to_atom(delete))
-      true -> nil
+    if  cron != nil and song != nil do
+      case Crontab.CronExpression.Parser.parse(cron) do
+        {:ok, expr} ->
+          if song in Alarmclock.Song.songs(), do: add_alarm(expr, song)
+      end
     end
-    f = render_html()
-    send_resp(conn, 200, f)
+    conn
+   |> put_resp_header("location", "/")
+   |> send_resp(302, "")
+  end
+
+  post "/api/del" do
+    delete = conn.params["delete"]
+    Alarmclock.Scheduler.delete_job(String.to_atom(delete))
+    conn
+    |> put_resp_header("location", "/")
+    |> send_resp(302, "")
   end
 
   def add_alarm(cron, song) do
-    n = Alarmclock.Scheduler.jobs() |> Enum.count()
+    name =
+      :crypto.hash(:blake2b, "#{System.os_time()}")
+      |> binary_slice(0..7)
+      |> Base.encode16()
+      |> String.to_atom() # potential OOM
     Alarmclock.Scheduler.new_job()
-    |> Quantum.Job.set_name(:"#{n}")
+    |> Quantum.Job.set_name(name)
     |> Quantum.Job.set_schedule(cron)
     |> Quantum.Job.set_task(fn -> Alarmclock.Song.play_song(song) end)
     |> Alarmclock.Scheduler.add_job()
@@ -48,6 +58,7 @@ defmodule Alarmclock.Router do
   end
 
   match _ do
+    IO.inspect(conn)
     send_resp(conn, 404, "404")
   end
 end
